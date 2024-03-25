e1e9cde9a772fb9935511145d26b0ab93e06f534
Matuksa <matuxapankevich@gmail.com>
2024-03-25 16:53:58 +0100

    Clear comments

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -9,8 +9,6 @@ import { Router } from '@angular/router';
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-  // public userName: string = '';
-  // public userPassword: string = '';
   registerForm: FormGroup = this.formBuilder.group({
     userName: ['', Validators.required],
     userPassword: ['', Validators.required],
@@ -20,39 +18,12 @@ export class LoginPageComponent {
     private formBuilder: FormBuilder,
     private router: Router,
   ) {}
-  // setUserName(userName: string): void {
-  //   this.userName = userName;
-  // }
-  // setUserPassword(userPassword: string): void {
-  //   this.userName = userPassword;
-  // }
-  // getUserName(): string {
-  //   return this.userName;
-  // }
-  // getUserPassword(): string {
-  //   return this.userName;
-  // }
+
   func(): void {
     console.log(this.registerForm.get);
   }
 
-  // email = new FormControl('', [Validators.required, Validators.email]);
-
-  // getErrorMessage() {
-  //   if (this.email.hasError('required')) {
-  //     return 'You must enter a value';
-  //   }
-
-  //   if (this.password.hasError('required')) {
-  //     return 'You must enter a value';
-  //   }
-
-  //   return this.email.hasError('email') ? 'Not a valid email' : '';
-  // }
-
   hide = true;
-
-  // password = new FormControl('', [Validators.required]);
 
   logIn() {
     const user = this.authService.logIn(
